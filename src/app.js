88498970d2b486fddfd0a35d4156751c005cf4c1
luiscastillaPro <159273408+luiscastillaPro@users.noreply.github.com>
2024-03-28 19:45:42 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
@@ -6,6 +5,28 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("generarexcusa").addEventListener("click", () => {
+    document.getElementById("excusa").innerHTML = generarescusa();
+  });
 };
+
+let who = ["el perro", "mi abuela", "el cartero", "mi pajaro"];
+let action = ["comio", "piso", "aplasto", "rompio"];
+let what = ["mi tarea", "mi telefono", "el auto", "la jaula"];
+let when = [
+  "antes de la clase",
+  "cuando dormia",
+  "mientras hacia ejercicio",
+  "durante mi almuerzo",
+  "mientras rezaba"
+];
+
+function generarescusa() {
+  let randomwho = who[Math.floor(Math.random() * who.length)];
+  let randomaction = action[Math.floor(Math.random() * action.length)];
+  let randomwhat = what[Math.floor(Math.random() * what.length)];
+  let randomwhen = when[Math.floor(Math.random() * when.length)];
+  const excusaaleatoria =
+    "${randomwho} ${randomaction} ${randomwhat} ${randomwhen}";
+  return excusaaleatoria;
+}
